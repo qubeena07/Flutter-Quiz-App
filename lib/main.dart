223206e6cd8045b07af47057ba4168d0fdb51dbf
Dipika Ranabhat <qubeena7@gmail.com>
2022-10-10 12:09:54 +0545

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,11 @@ import 'package:quiz_app/utils/routes/routes.dart';
 import 'package:quiz_app/utils/routes/routes_name.dart';
 import 'package:quiz_app/view%20/onboarding_screen.dart';
 import 'package:quiz_app/view_model/question_answer_view_model.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
